122f79250550610a369933e61b077dc6eee462cf
Janschuri <124630782+Janschuri@users.noreply.github.com>
2025-03-27 16:45:52 +0100

    default messages

--- src/main/java/de/janschuri/SurvivalDebugStick/commands/Subcommand.java
+++ src/main/java/de/janschuri/SurvivalDebugStick/commands/Subcommand.java
@@ -12,9 +12,15 @@ import javax.naming.CompositeName;
 
 public abstract class Subcommand extends LunaticCommand {
 
-    protected static final MessageKey NO_PERMISSION_MK = new LunaticMessageKey("no_permission");
-    protected static final MessageKey WRONG_USAGE_MK = new LunaticMessageKey("wrong_usage");
-    protected static final MessageKey NO_CONSOLE_COMMAND_MK = new LunaticMessageKey("no_console_command");
+    protected static final MessageKey NO_PERMISSION_MK = new LunaticMessageKey("no_permission")
+            .defaultMessage("en", "You don't have permission to execute this command.")
+            .defaultMessage("de", "Du hast keine Berechtigung, diesen Befehl auszuführen.");
+    protected static final MessageKey WRONG_USAGE_MK = new LunaticMessageKey("wrong_usage")
+            .defaultMessage("en", "Wrong usage! Please check the command syntax.")
+            .defaultMessage("de", "Falsche Verwendung! Bitte überprüfe die Befehlsyntax.");
+    protected static final MessageKey NO_CONSOLE_COMMAND_MK = new LunaticMessageKey("no_console_command")
+            .defaultMessage("en", "This command can only be executed by a player.")
+            .defaultMessage("de", "Dieser Befehl kann nur von einem Spieler ausgeführt werden.");
     protected static final MessageKey PAGE_MK = new LunaticMessageKey("page")
             .defaultMessage("en", "Page")
             .defaultMessage("de", "Seite");
